de9195064689731d4b264923e4b69466bea2b947
Julia Skip <skipjulija01@gmail.com>
2026-02-20 14:18:54 +0200

    Initial Commit

---
+++ Skip05/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Skip05
+//
+//  Created by Скіп Юлія Ярославівна on 20.02.2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Skip05/Skip05App.swift
@@ -0,0 +1,17 @@
+//
+//  Skip05App.swift
+//  Skip05
+//
+//  Created by Скіп Юлія Ярославівна on 20.02.2026.
+//
+
+import SwiftUI
+
+@main
+struct Skip05App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
